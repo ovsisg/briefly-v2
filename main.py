b06f73304613bd74497320f6a954c95688a32025
ovsisg <ovsisg@gmail.com>
2026-08-07 00:10:01 +0200

    Create news summariser

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-from dotenv import load_dotenv
+import requests
 import os
+from dotenv import load_dotenv
+from send_email import send_email
 from langchain.chat_models import init_chat_model
+
 
 load_dotenv()
 
-api_key = os.getenv("GOOGLE_API_KEY")
+news_api_key = os.getenv("NEWS_API_KEY")
+google_api_key = os.getenv("GOOGLE_API_KEY")
+
+url = (
+    "https://newsapi.org/v2/top-headlines?"
+    "category=business&"
+    "language=en&"
+    "pageSize=8&"
+    "sortBy=publishedAt&apiKey=" + news_api_key
+)
+
+request = requests.get(url)
+content = request.json()
+articles = content["articles"]
 
 model = init_chat_model(
     model="gemini-3-flash-preview",
     model_provider="google-genai", 
-    api_key=api_key)
+    api_key=google_api_key)
 
-result = model.invoke("What colour is an orange?")
+prompt = f"""
+    You're a business news summariser.
+    Write a short paragraph analysing these news articles.
+    Focus on the most important stories and explain why they matter and how they could affect businesses, markets, or consumers.
+    Mention key companies, industries, or trends where relevant.
+    Do not add information that is not in the articles.
+    Do not use Markdown formatting.
+    Here are the news articles:
+    {articles}
+"""
+
+result = model.invoke(prompt)
 reply = result.content[0]["text"]
-print(reply)
+reply = reply.replace("**", "")
+
+body = "Subject: Latest Business News\n\n" + reply + "\n\n"
+
+body = body.encode("utf-8")
+send_email(body)
