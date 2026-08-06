cd2299e337739632eb88cebf5160f8b7b8a31570
ovsisg <ovsisg@gmail.com>
2026-08-06 20:44:52 +0200

    Set up LangChain with Gemini

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from dotenv import load_dotenv
+import os
+from langchain.chat_models import init_chat_model
 
 load_dotenv()
 
+api_key = os.getenv("GOOGLE_API_KEY")
+
+model = init_chat_model(
+    model="gemini-3-flash-preview",
+    model_provider="google-genai", 
+    api_key=api_key)
+
+result = model.invoke("What colour is an orange?")
+reply = result.content[0]["text"]
+print(reply)
